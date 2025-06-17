e3996e6ec988835c380c864691005ca6d145d981
Sincerity Egbu <sincerityegbu@gmail.com>
2025-06-17 11:04:36 +0100

    Added colour variables

---
+++ toDay/styles/Colours.ts
@@ -0,0 +1,10 @@
+export const colours = {
+    primary: "#07008a", 
+    secondary: "#040048", 
+    accent: "#0a03e8",
+    error: "#e80338", 
+    success: "#03e84c",
+    text: "#e8e5ff",
+    textDim: "#868590",
+    background: "#050518"  
+}
